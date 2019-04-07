f3b1c1c5b9800e9ef7e98c9d81b12319ac4f5f70
Andrej Noskaj <andrej.nosky@gmail.com>
2019-04-07 03:29:06 +0200

    Fixed mongo driver.

--- core/settings.js
+++ core/settings.js
@@ -4,6 +4,6 @@
 
 module.exports = {
     port: '7787',
-    // dbUrl: 'mongodb://jsonstream_admin:rbSsq667@ds237373.mlab.com:37373/heroku_q12mvj12',
-    dbUrl: 'mongodb://localhost:27017/general_storage'
+    dbUrl: 'mongodb://jsonstream_admin:rbSsq667@ds237373.mlab.com:37373/heroku_q12mvj12',
+    // dbUrl: 'mongodb://localhost:27017/general_storage'
 }
