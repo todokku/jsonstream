44661c8e016e60e2a99955f95824160a4cc4a293
Andrej Noskaj <andrej.nosky@gmail.com>
2019-08-03 15:39:16 +0200

    Added workbox.

--- fe/dist/service-worker.js
+++ fe/dist/service-worker.js
@@ -3,7 +3,28 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox
 if (workbox) {
 
   // Manifest generator.
-  workbox.precaching.precacheAndRoute([]);
+  workbox.precaching.precacheAndRoute([
+  {
+    "url": "css/app.42d1d523.css",
+    "revision": "0eee7498ee5504284a0915e83e7f95ea"
+  },
+  {
+    "url": "favicon.ico",
+    "revision": "b32d14b29042bdfec6260b3183098e05"
+  },
+  {
+    "url": "index.html",
+    "revision": "86a22fb6163c72b8748ef7f95a5c1f4f"
+  },
+  {
+    "url": "js/app.9f8f7049.js",
+    "revision": "c03298cd6a666f4457bceef02f87477c"
+  },
+  {
+    "url": "js/chunk-vendors.8a1b2796.js",
+    "revision": "1c02b0241b34f8f474468a01159fd24a"
+  }
+]);
 
   // Cache JS and CSS files.
   workbox.routing.registerRoute(
